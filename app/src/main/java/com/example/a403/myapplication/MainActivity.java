349a07be22cb94d46065a5351366140ec2e7ef44
myname <myname@test.com>
2016-11-11 19:16:29 +0900

    레이팅바

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.a403.myapplication;
 
+import android.graphics.Color;
+import android.media.Rating;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout layout;
+    Button b1;
+    CheckBox checkbox1;
+    RatingBar rb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 =(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkbox1 = (CheckBox)findViewById(R.id.checkbox);
+        checkbox1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkbox1.isChecked())
+                    layout.setBackgroundColor(Color.BLUE);
+                else
+                    layout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+        rb1=(RatingBar)findViewById(R.id.ratingBar);
+        rb1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
+            }
+        });
+        });
     }
 }
